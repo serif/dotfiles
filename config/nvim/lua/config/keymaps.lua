a002d9ba22d3ec6b4158ba44bf4da2ee2b364a84
serif <serif@users.noreply.github.com>
2023-07-27 01:14:25 -0400

    vim revival

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,18 +3,44 @@
 -- Add any additional keymaps here
 
 local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
+	local keys = require("lazy.core.handler").handlers.keys
+	---@cast keys LazyKeysHandler
+	-- do not create the keymap if a lazy keys handler exists
+	if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+		opts = opts or {}
+		opts.silent = opts.silent ~= false
+		vim.keymap.set(mode, lhs, rhs, opts)
+	end
 end
 
 -- Quick return to normal mode
 map("i", ",n", "<esc>l")
+map("v", ",n", "<esc>l")
 
 -- Clear current search highlight
 map("n", "//", ":noh<CR>")
+
+-- Raven layout
+map("n", "n", "gj")
+map("n", "e", "gk")
+map("n", "a", "l")
+map("n", "t", "i")
+map("n", "T", "I")
+map("n", "E", "e")
+map("n", "i", "a")
+map("n", "h", "n")
+map("n", "H", "N")
+map("n", "<C-n>", "<C-f>")
+map("n", "<C-e>", "<C-b>")
+
+map("v", "n", "gj")
+map("v", "e", "gk")
+map("v", "a", "l")
+map("v", "t", "i")
+map("v", "T", "I")
+map("v", "E", "e")
+map("v", "i", "a")
+map("v", "h", "n")
+map("v", "H", "N")
+map("v", "<C-n>", "<C-f>")
+map("v", "<C-e>", "<C-b>")
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-  { "doums/darcula" },
+	-- Alternates
+	{ "ellisonleao/gruvbox.nvim" },
+	{ "doums/darcula" },
+	-- nope { "freddiehaddad/base16-nvim" },
+	-- nope { "Soares/base16.nvim" },
+	-- nope { "RRethy/nvim-base16" },
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "darcula",
-    },
-  },
+	-- Configure LazyVim to load colorscheme
+	{
+		"LazyVim/LazyVim",
+		opts = { colorscheme = "darcula" },
+	},
 }
