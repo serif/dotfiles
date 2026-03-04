011e317ec61ecffaa4735905c45de956a2ff806a
serif <62613995+serif@users.noreply.github.com>
2026-03-03 22:37:32 -0500

    consolidate deployment
    consolidate deployment scripts

--- config/nvim/old/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
--- Fix 'unable to install parser' bug
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.ignore_install = { "help" }
-    end,
-  },
-}
--- fonts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p $HOME/.local/share/
-ln -s "$(pwd)/fonts" $HOME/.local/share/
-fc-cache -f -v
-
